514c5a2ae9f9331a13bf911f2d3b6b7ab80c0a2b
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2022-11-14 18:41:21 +0100

    added the speech recognition capabilities.
    -system can as well now convert speech to text

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import speech_recognition as virtual_assistant
 
+listener = virtual_assistant.Recognizer()
 
+try:
+    #file_handling: helps to open and close the file function.
+    with virtual_assistant.Microphone() as source:
+        print("what do you help with?...")
+        voice_command = listener.listen(source)
+        command = listener.recognize_google(voice_command)
+        print(command)
+except:
+    pass
